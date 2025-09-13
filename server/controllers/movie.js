e5e7b988e09dc030d09a88b6826fc6227dc2230b
Purvi Murkute <purvimurkute51@gmail.com>
2025-09-13 14:39:49 +0530

    get movie by id implement

--- server/controllers/movie.js
+++ server/controllers/movie.js
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 import { getCache, createCache } from "../utils/cache.js";
 
+const API_KEY = process.env.TMDB_API_KEY;
+console.log(API_KEY);
+
 const getmovies = async (req, res) => {
   try {
     const { page = 1, query } = req.query;
-    const API_KEY = process.env.TMDB_API_KEY;
-    console.log(API_KEY);
 
     let tmdbUrl;
     if (query) {
@@ -70,4 +71,79 @@ const getmovies = async (req, res) => {
   }
 };
 
-export { getmovies };
+const getMoviebyId = async (req, res) => {
+  const { movieId } = req.params;
+
+  const cacheKey = `movie:details:${movieId}`;
+
+  try {
+    const cachedData = await getCache(cacheKey);
+    if (cachedData) {
+      return res.status(200).json({
+        success: true,
+        data: cachedData,
+        message: "Movie data fetched successfully (from cache)",
+      });
+    }
+
+    const tmdbURL1 = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+    const tmdbURL2 = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`;
+    const tmdbURL3 = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`;
+
+    const fetchWithTimeout = (url, options = {}, timeout = 10000) =>
+      Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+          setTimeout(() => reject(new Error("Request timed out")), timeout)
+        ),
+      ]);
+
+    const movieInfoRes = await fetchWithTimeout(tmdbURL1);
+    const castAndCrewRes = await fetchWithTimeout(tmdbURL2);
+    const trailerRes = await fetchWithTimeout(tmdbURL3);
+
+    const movieInfo = await movieInfoRes.json();
+    const castandcrew = await castAndCrewRes.json();
+    const trailer = await trailerRes.json();
+
+    const directorDoc = castandcrew?.crew?.find(
+      (person) => person.job === "Director"
+    );
+    const cast = castandcrew?.cast?.map((member) => ({
+      name: member.name,
+      character: member.character,
+      profile: member.profile_path,
+    }));
+    const trailerKey = trailer?.results[0]?.key;
+
+    const data = {
+      title: movieInfo?.title,
+      releaseDate: movieInfo?.release_date,
+      poster: `https://image.tmdb.org/t/p/w500/${movieInfo?.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w780/${movieInfo?.backdrop_path}`,
+      genre: movieInfo?.genres.map((genre) => genre.name),
+      overview: movieInfo?.overview,
+      director: directorDoc?.name,
+      cast: cast,
+      trailer: trailerKey
+        ? `https://www.youtube.com/embed/${trailerKey}`
+        : null,
+    };
+
+    await createCache(cacheKey, data);
+
+    return res.status(200).json({
+      success: true,
+      data: data,
+      message: "Movie data fetched successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      data: null,
+      message: error?.message || "Failed to fetch movie data",
+    });
+  }
+};
+
+export { getmovies, getMoviebyId };
--- server/routes/movieRoute.js
+++ server/routes/movieRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getmovies } from '../controllers/movie.js';
+import { getMoviebyId, getmovies } from '../controllers/movie.js';
 import { verifyJWT } from '../middlewares/jwt.js';
 import { getReviewsByMovie, postReviews } from '../controllers/review.js';
 
@@ -8,5 +8,6 @@ const movieRouter = express.Router();
 movieRouter.get('/movies', getmovies);
 movieRouter.post('/movies/:movieId/reviews', verifyJWT, postReviews);
 movieRouter.get('/movies/:movieId/reviews', getReviewsByMovie);
+movieRouter.get('/movies/:movieId', getMoviebyId);
 
 export default movieRouter;
